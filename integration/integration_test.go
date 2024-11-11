eb3c137d185c2a931b6a581dac91fbf2810841de
黄少威 <huangshaowei@xmfunny.com>
2024-11-11 14:05:52 +0800

    fix(funnydb-go-sdk): 修复 funnydb-go-sdk 由于服务端异常导致退出 #15

--- integration/integration_test.go
+++ integration/integration_test.go
@@ -5,21 +5,16 @@ import (
 	"fmt"
 	sdk "git.sofunny.io/data-analysis/funnydb-go-sdk"
 	"git.sofunny.io/data-analysis/funnydb-go-sdk/internal"
-	client "git.sofunny.io/data-analysis/ingest-client-go-sdk"
-	"github.com/h2non/gock"
-	jsoniter "github.com/json-iterator/go"
 	"github.com/stretchr/testify/assert"
-	"io"
-	"net/http"
 	"os"
-	"strings"
 	"testing"
 	"time"
 )
 
+var userLoginEventName = "UserLogin"
 var userLoginEvent = &sdk.Event{
 	Time: time.Now(),
-	Name: "UserLogin",
+	Name: userLoginEventName,
 	Props: map[string]interface{}{
 		"#account_id": "account-fake955582",
 		"#channel":    "tapdb",
@@ -27,51 +22,10 @@ var userLoginEvent = &sdk.Event{
 	},
 }
 
-var singleMessageMatcher = gock.NewBasicMatcher()
-var doubleMessageMatcher = gock.NewBasicMatcher()
-
-func init() {
-	singleMessageMatcher.Add(func(req *http.Request, ereq *gock.Request) (bool, error) {
-		return checkRequestBody(req, 1)
-	})
-	doubleMessageMatcher.Add(func(req *http.Request, ereq *gock.Request) (bool, error) {
-		return checkRequestBody(req, 2)
-	})
-}
-
-func checkRequestBody(req *http.Request, msgSize int) (bool, error) {
-	bytes, matcherErr := io.ReadAll(req.Body)
-	if matcherErr != nil {
-		return false, matcherErr
-	}
-
-	gunzipData, unzipErr := internal.GunzipData(bytes)
-	if unzipErr != nil {
-		return false, unzipErr
-	}
-
-	var batch client.Messages
-	unmarshalErr := jsoniter.Unmarshal(gunzipData, &batch)
-	if unmarshalErr != nil {
-		return false, unmarshalErr
-	}
-
-	if len(batch.Messages) == msgSize {
-		return true, nil
-	}
-
-	return false, nil
-}
-
-func createGockReq() *gock.Request {
-	return gock.New("http://ingest.com").
-		Post("/v1/collect")
-}
-
 func createClient(tmpDir string) (*sdk.Client, error) {
 	config := &sdk.Config{
 		Mode:           sdk.ModeAsync,
-		IngestEndpoint: "http://ingest.com",
+		IngestEndpoint: internal.IngestMockEndpoint, // 该地址不会创建统计上报影响测试结果
 		SendTimeout:    5 * time.Second,
 		AccessKey:      "demo",
 		AccessSecret:   "demo",
@@ -80,48 +34,36 @@ func createClient(tmpDir string) (*sdk.Client, error) {
 	return sdk.NewClient(config)
 }
 
-func waitingForResponse() {
-	for {
-		if gock.IsDone() {
-			break
-		}
-
-		time.Sleep(50 * time.Millisecond)
-	}
-}
-
 // 基础使用测试
 func TestAsyncClient(t *testing.T) {
-	tmpDir, err := os.MkdirTemp("", fmt.Sprintf("client-async-test-%d", time.Now().UnixNano()))
-	assert.Nil(t, err)
+	tmpDir := fmt.Sprintf("/tmp/client-async-test-%d", time.Now().UnixNano())
 	defer os.RemoveAll(tmpDir)
 
 	c, err := createClient(tmpDir)
 	assert.Nil(t, err)
 
-	createGockReq().
+	internal.CreateMockCollectGockReq().
 		Reply(200).
 		JSON(map[string]interface{}{"error": nil})
 
 	err = c.ReportEvent(context.Background(), userLoginEvent)
 	assert.Nil(t, err)
 
-	waitingForResponse()
+	internal.WaitingForGockDone(t)
 
 	c.Close(context.Background())
 }
 
 // 测试正常重启后数据不会重复发送
 func TestAsyncClientNormalRestart(t *testing.T) {
-	tmpDir, err := os.MkdirTemp("", fmt.Sprintf("client-async-test-%d", time.Now().UnixNano()))
-	assert.Nil(t, err)
+	tmpDir := fmt.Sprintf("/tmp/client-async-test-%d", time.Now().UnixNano())
 	defer os.RemoveAll(tmpDir)
 
 	c, err := createClient(tmpDir)
 	assert.Nil(t, err)
 
-	createGockReq().
-		SetMatcher(singleMessageMatcher).
+	internal.CreateMockCollectGockReq().
+		SetMatcher(internal.OneMessageSizeMatcher).
 		Times(1).
 		Reply(200).
 		JSON(map[string]interface{}{"error": nil})
@@ -129,12 +71,12 @@ func TestAsyncClientNormalRestart(t *testing.T) {
 	err = c.ReportEvent(context.Background(), userLoginEvent)
 	assert.Nil(t, err)
 
-	waitingForResponse()
+	internal.WaitingForGockDone(t)
 
 	c.Close(context.Background())
 
-	createGockReq().
-		SetMatcher(singleMessageMatcher).
+	internal.CreateMockCollectGockReq().
+		SetMatcher(internal.OneMessageSizeMatcher).
 		Times(1).
 		Reply(200).
 		JSON(map[string]interface{}{"error": nil})
@@ -145,95 +87,96 @@ func TestAsyncClientNormalRestart(t *testing.T) {
 	err = c.ReportEvent(context.Background(), userLoginEvent)
 	assert.Nil(t, err)
 
-	waitingForResponse()
+	internal.WaitingForGockDone(t)
 
 	c.Close(context.Background())
 }
 
-// 测试异常重启后数据正确发送
+// 测试数据异常丢弃该批次数据
 func TestAsyncClientAuthErrorRestart(t *testing.T) {
-	tmpDir, err := os.MkdirTemp("", fmt.Sprintf("client-async-test-%d", time.Now().UnixNano()))
-	assert.Nil(t, err)
+	tmpDir := fmt.Sprintf("/tmp/client-async-test-%d", time.Now().UnixNano())
 	defer os.RemoveAll(tmpDir)
 
 	c, err := createClient(tmpDir)
 	assert.Nil(t, err)
 
-	createGockReq().
-		SetMatcher(singleMessageMatcher).
+	internal.CreateMockCollectGockReq().
+		SetMatcher(internal.OneMessageSizeMatcher).
 		Times(1).
-		Reply(401).
-		JSON(map[string]interface{}{"error": "Unauthorized"})
+		Reply(400).
+		JSON(map[string]interface{}{"error": "Bad Request"})
 
 	err = c.ReportEvent(context.Background(), userLoginEvent)
 	assert.Nil(t, err)
 
-	waitingForResponse()
+	internal.WaitingForGockDone(t)
 
-	// 等待 client 关闭
-	time.Sleep(3 * time.Second)
-
-	err = c.Close(context.Background())
-	assert.NotNil(t, err)
-	assert.True(t, strings.Contains(err.Error(), "Unauthorized"))
-
-	createGockReq().
-		SetMatcher(doubleMessageMatcher).
+	internal.CreateMockCollectGockReq().
+		SetMatcher(internal.OneMessageSizeMatcher).
 		Times(1).
 		Reply(200).
 		JSON(map[string]interface{}{"error": nil})
 
-	c, err = createClient(tmpDir)
-	assert.Nil(t, err)
-
 	err = c.ReportEvent(context.Background(), userLoginEvent)
 	assert.Nil(t, err)
 
-	waitingForResponse()
+	internal.WaitingForGockDone(t)
 
 	c.Close(context.Background())
 }
 
-// 测试异常重启后数据正确发送
+// 测试 ingest 服务异常，过一段时间恢复后数据正确发送
 func TestAsyncClientServerErrorRestart(t *testing.T) {
-	tmpDir, err := os.MkdirTemp("", fmt.Sprintf("client-async-test-%d", time.Now().UnixNano()))
-	assert.Nil(t, err)
+	tmpDir := fmt.Sprintf("/tmp/client-async-test-%d", time.Now().UnixNano())
 	defer os.RemoveAll(tmpDir)
 
 	c, err := createClient(tmpDir)
 	assert.Nil(t, err)
 
-	createGockReq().
-		SetMatcher(singleMessageMatcher).
-		Times(5).
+	internal.CreateMockCollectGockReq().
+		SetMatcher(internal.OneMessageSizeMatcher).
+		Times(12).
 		Reply(500).
 		JSON(map[string]interface{}{"error": "ServerInternalError"})
 
 	err = c.ReportEvent(context.Background(), userLoginEvent)
 	assert.Nil(t, err)
 
-	waitingForResponse()
-
-	// 等待 client 关闭
-	time.Sleep(3 * time.Second)
-
-	err = c.Close(context.Background())
-	assert.NotNil(t, err)
-	assert.True(t, strings.Contains(err.Error(), "context deadline exceeded"))
-
-	createGockReq().
-		SetMatcher(doubleMessageMatcher).
-		Times(1).
-		Reply(200).
-		JSON(map[string]interface{}{"error": nil})
-
-	c, err = createClient(tmpDir)
-	assert.Nil(t, err)
+	internal.WaitingForGockDone(t)
 
 	err = c.ReportEvent(context.Background(), userLoginEvent)
 	assert.Nil(t, err)
 
-	waitingForResponse()
+	internal.CreateMockCollectGockReq().
+		SetMatcher(internal.OneMessageSizeMatcher).
+		Times(2).
+		Reply(200).
+		JSON(map[string]interface{}{"error": nil})
+
+	internal.WaitingForGockDone(t)
+
+	c.Close(context.Background())
+}
+
+// 测试 ingest 服务异常过程中程序正常关闭
+func TestAsyncClientServerErrorStop(t *testing.T) {
+	tmpDir := fmt.Sprintf("/tmp/client-async-test-%d", time.Now().UnixNano())
+	defer os.RemoveAll(tmpDir)
+
+	c, err := createClient(tmpDir)
+	assert.Nil(t, err)
+
+	internal.CreateMockCollectGockReq().
+		SetMatcher(internal.OneMessageSizeMatcher).
+		Times(100).
+		Reply(500).
+		JSON(map[string]interface{}{"error": "ServerInternalError"})
+
+	err = c.ReportEvent(context.Background(), userLoginEvent)
+	assert.Nil(t, err)
+
+	// 等待业务数据发送
+	time.Sleep(10 * time.Second)
 
 	c.Close(context.Background())
 }
