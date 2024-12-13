fbcb73aed4e2e15016f0fcb1d2881586ce2a242e
lizeqing <lizeqing@xmfunny.com>
2024-12-13 16:38:22 +0800

    misc: bump up version

--- internal/producer.go
+++ internal/producer.go
@@ -11,7 +11,7 @@ const (
 	EventTypeValue = "Event"
 
 	SdkType    = "go-sdk"
-	SdkVersion = "1.2.3"
+	SdkVersion = "2.0.2"
 
 	DataFieldNameSdkType    = "#sdk_type"
 	DataFieldNameSdkVersion = "#sdk_version"
